1acd5fe3588a880a7cd295d1617f7aaae63c9629
Darragh Ferry <darraghferry@hotmail.com>
2020-01-15 20:26:15 +0000

    created get enpoints for /api/users and /api/users/:id/posts

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
+const express = require("express");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+server.use(logger);
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.post("/", (req, res) => {
+  res.send("hello");
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`METHOD: ${req.method} URL: ${req.url} ${new Date()}`);
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,49 @@
-const express = require('express');
+const express = require("express");
+
+const Users = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  Users.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "couldn't get users", error: err });
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", (req, res) => {
+  // do your magic!
+  console.log(req.params);
+  Users.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong", error: err });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
