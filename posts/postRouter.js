373393fca1edb387447894981b9bf9a777d66c4d
Darragh Ferry <darraghferry@hotmail.com>
2020-01-16 12:51:10 +0000

    created get endpoints for posts and getById

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,36 @@
-const express = require('express');
+const express = require("express");
+
+const Posts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  Posts.get()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong", error: err });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  // do your magic!
+  Posts.getById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "something went wrong", error: err });
+    });
+});
+
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
